ee3cc7c6e5707094edce565c27069f7980591df9
Sander W. van der Laan <s.w.vanderlaan@gmail.com>
2018-03-20 21:50:39 +0100

    Committing the (updated) source files.

--- deploy.sh
+++ deploy.sh
@@ -13,12 +13,11 @@ function echoitalic { #'echobold' is the function name
 
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echobold "                              UPDATING WEBSITE"
-echo "                                    version 1.2"
 echo ""
 echoitalic "* Written by  : Sander W. van der Laan"
 echoitalic "* E-mail      : s.w.vanderlaan-2@umcutrecht.nl"
-echoitalic "* Last update : 2017-10-04"
-echoitalic "* Version     : v1.2"
+echoitalic "* Last update : 2018-03-20"
+echoitalic "* Version     : v1.3"
 echo ""
 echoitalic "* Description : This script will set some directories, execute some things, "
 echoitalic "                and will then update the website."
@@ -37,7 +36,7 @@ USERROOT="/Users/swvanderlaan"
 ROOT="${USERROOT}/website"
 
 WEBSITE="${ROOT}/mywebsite-hugo"
-WEBSITEPUBLIC="${ROOT}/swvanderlaan.github.io"
+WEBSITEPUBLIC="${WEBSITE}/public"
 
 echo "Root directory____________ ${ROOT}"
 echo "Website root directory____ ${WEBSITE}"
@@ -55,47 +54,47 @@ git status
 git add -A
 git commit -m "Committing the (updated) source files."
 git push origin master
-
-# if using a theme, replace by `hugo -t <yourtheme>`
-hugo 
-
-### Depending on your needs, you can create a LICENSE file and README.md. You can use
-### these as an example 
-cp -v ${ROOT}/focus_stuff/LICENSE.md ${WEBSITEPUBLIC}/LICENSE.md
-cp -v ${ROOT}/focus_stuff/README.md ${WEBSITEPUBLIC}/README.md
-### If you want to re-direct your GitHub page to another domain, you'll have to make a
-### 'CNAME' file.
-# echo "http://www.waldamargroup.com" > ${WEBSITEPUBLIC}/CNAME
-
-echo ""
-echo "-----------------------------"
-echo "* Going to 'public' folder..."
-
-cd ${ROOT}/swvanderlaan.github.io
-
-cp -afv ${ROOT}/mywebsite-hugo/public/* .
-
-echo ""
-echo "-----------------------------"``
-echo "* Add changes to git..."
-
-git status
-git add -A
-
-echo ""
-echo "-----------------------------"`
-echo "* Commit changes..."
-msg="> Rebuilding site $(date)."
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
-git commit -m "$msg."
-
-# Push source and build repos.
-git push origin master
-
-# Come Back
-cd ${ROOT}
+# 
+# ### if using a theme, replace by 'hugo -t <yourtheme>'
+# hugo 
+# 
+# ### Depending on your needs, you can create a LICENSE file and README.md. You can use
+# ### these as an example 
+# cp -v ${ROOT}/focus_stuff/LICENSE.md ${WEBSITEPUBLIC}/LICENSE.md
+# cp -v ${ROOT}/focus_stuff/README.md ${WEBSITEPUBLIC}/README.md
+# ### If you want to re-direct your GitHub page to another domain, you'll have to make a
+# ### 'CNAME' file.
+# # echo "http://www.waldamargroup.com" > ${WEBSITEPUBLIC}/CNAME
+# 
+# echo ""
+# echo "-----------------------------"
+# echo "* Going to 'public' folder..."
+# 
+# cd ${ROOT}/swvanderlaan.github.io
+# 
+# cp -afv ${ROOT}/mywebsite-hugo/public/* .
+# 
+# echo ""
+# echo "-----------------------------"``
+# echo "* Add changes to git..."
+# 
+# git status
+# git add -A
+# 
+# echo ""
+# echo "-----------------------------"`
+# echo "* Commit changes..."
+# msg="> Rebuilding site $(date)."
+# if [ $# -eq 1 ]
+#   then msg="$1"
+# fi
+# git commit -m "$msg."
+# 
+# # Push source and build repos.
+# git push origin master
+# 
+# # Come Back
+# cd ${ROOT}
 
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echobold "Wow. We're all done. Let's have a 🍺 !"
